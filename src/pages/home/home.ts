2641cd4f9f63d66c2fb0b22aa697d686ba38936f
Ronit Saha <saharanit321@gmail.com>
2018-08-04 12:54:48 +0530

    modified application

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RestApiProvider } from '../../providers/rest-api/rest-api';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  countries: string[];
+  errorMessage: string;
+  descending: boolean = false;
+  order: number;
+  column: string = 'name';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public rest: RestApiProvider) {
 
+  }
+
+  getCountries() {
+    this.rest.getCountries()
+       .subscribe(
+         countries => this.countries = countries,
+         error =>  this.errorMessage = <any>error);
+  }
+
+  ionViewDidLoad() {
+    this.getCountries();
+  }
+
+  sort(){
+    this.descending = !this.descending;
+    this.order = this.descending ? 1 : -1;
   }
 
 }
